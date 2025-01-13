4a5a860dcc8ff7c84ce9d465e373c03ca38acb21
Anna <46578108+anchsk@users.noreply.github.com>
2025-01-14 00:08:57 +0100

    modal mobile align left

--- src/ui/product-page.tsx
+++ src/ui/product-page.tsx
@@ -13,40 +13,28 @@ export default function ProductPage(props: {
     <div
       className={clsx('relative px-4 md:py-4', {
         'block mb-20 md:h-screen': !isModal,
-        'pt-16 md:pt-4 h-full': isModal
+        'pt-16 md:pt-4 h-full px-4 md:px0': isModal
       })}
     >
       <div
         className={clsx(
           '_image-container block relative aspect-[0.7] md:h-full mx-auto',
           {
-            'h-[70svh]': isModal
+            'w-full h-[unset] md:w-[unset] ': isModal
           }
         )}
       >
         <ImageMedia asset={product?.mainImage?.asset}/>
       </div>
 
-      {/*       {product?.title && (
-        <div className="block absolute bottom-4 left-4 leading-[0.75]">
-          {product.title}
-        </div>
-      )}
-
-      {product?.price && (
-        <div className="block absolute bottom-4 right-4 leading-[0.75]">
-          {euro.format(product.price)}
-        </div>
-      )} */}
       <div
         className={clsx(
           'pt-4 md:leading-[0.75] md:pt-0 md:absolute md:bottom-4 md:left-4 md:right-4',
-          'flex flex-col items-center md:flex-row md:justify-between',
-          
+          'flex flex-col items-start md:flex-row md:justify-between',
         )}
       >
-        <div>Title</div>
-        <div>40</div>
+        {product?.title && <h1>{product.title}</h1>}
+        {product?.price && <div>{euro.format(product.price)}</div>}
       </div>
     </div>
   );
