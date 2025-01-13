1e3f9bb64ffeca904107d1149f33eccc5ce43c2b
Anna <46578108+anchsk@users.noreply.github.com>
2025-01-14 00:09:23 +0100

    header text align

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="w-full flex flex-col items-center p-4 shrink-0">
       <Link href="/">Pot Pot Pottery</Link>
-      <div className='text-[--font-gray]'>Potteries by Leonardo Romano</div>
+      <div className='text-[--font-gray] text-center'>Potteries by Leonardo Romano</div>
     </header>
   );
 }
