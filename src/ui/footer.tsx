300fc7d73869e22b055956c39a98656fa9847272
Anna <46578108+anchsk@users.noreply.github.com>
2025-01-14 18:38:25 +0100

    upd footer mobile

--- src/ui/footer.tsx
+++ src/ui/footer.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <footer
-      className="w-full h-full p-4
-  flex justify-between"
-    >
-      <div>Pot Pot Pottery</div>
-      <div className="flex flex-row gap-4 text-[--font-gray]">
+    <footer className="w-full h-full p-4 flex flex-col-reverse gap-10 md:flex-row md:justify-between">
+      <Link href="/" className="">
+        Pot Pot Pottery
+      </Link>
+      <div className="flex flex-col gap-4 md:flex-row md:gap-4 text-[--font-gray]">
         <a
           href="https://instagram.com/potpotpottery"
           target="_blank"
